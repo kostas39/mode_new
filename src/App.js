55ede93c84dd45309452bfe8c66cf6bc9becaf67
konstantinos koliolios <kolioliosk@gmail.com>
2023-06-02 15:08:15 +0100

    hero component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Hero.css";
+import heroImage from "../../assets/hero_image.jpeg"; // Assuming you have a hero image file named "hero_image.jpg" in the assets folder
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={heroImage} alt="Hero" className="hero-image" />
+      <div className="content">
+        <h1>Welcome to Mode Constructions Services Ltd</h1>
+        <p>Your trusted partner in construction projects</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
