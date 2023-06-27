81891ce1367bbf35047183def9ec3a9a7a2a8b09
konstantinos koliolios <kolioliosk@gmail.com>
2023-06-27 17:39:58 +0300

    changes in about us

--- src/components/AboutUsPage/AboutUsPage.js
+++ src/components/AboutUsPage/AboutUsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './AboutUsPage.css'
 
 const AboutUsPage = () => {
   const isAboutUsRoute = window.location.pathname === "/about-us";
@@ -9,7 +10,7 @@ const AboutUsPage = () => {
 
   return (
     <div>
-      <h1>MODE is a UK-based, construction services company which focuses mainly on residential and commercial building developments. The company has built reputation for providing reliable, dependable services for high-class quality buildings and provide solutions to complex requirements.
+      <h1 className="about_script">MODE is a UK-based, construction services company which focuses mainly on residential and commercial building developments. The company has built reputation for providing reliable, dependable services for high-class quality buildings and provide solutions to complex requirements.
 OUR MISSION
 To deliver high-quality, cost-effective projects on schedule, employing and supporting motivated, flexible and focused teams to achieve the best results. At Mode, we value the importance of our relationships, and we will continue to remain fair in our dealings with all employees, clients and suppliers.
 OUR VALUES
