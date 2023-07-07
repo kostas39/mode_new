594a725faf4b928fa3dacf0b908b7c65e99d0f8f
konstantinos koliolios <kolioliosk@gmail.com>
2023-07-07 18:49:55 +0100

    changes in animated

--- src/components/LatestProjects/LatestProjects.js
+++ src/components/LatestProjects/LatestProjects.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './LatestProjects.scss';
+import { useEffect, useState } from 'react';
 import Image1 from '../../assets/companyPhoto.jpeg';
 import Image2 from '../../assets/heroimage1.jpeg';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import './LatestProjects.scss';
+
 
 const LatestProjects = () => {
-  const strArray = ['Our', ' Latest', ' Projects'];
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
   return (
     <div className="latest-projects">
       <h2>
-        <AnimatedLetters letterClass="animated-letter" strArray={strArray} index={0} />
+      <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['O', 'u', 'r', ' ', 'L', 'a', 't', 'e', 's', 't', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+            index={30}
+          />
       </h2>
       <div className="project-cards">
         <div className="project-card">
