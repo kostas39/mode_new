7150588d244ea663f1e9e41a112a0df16aafb53e
konstantinos koliolios <kolioliosk@gmail.com>
2023-07-15 22:51:35 +0100

    commit

--- src/components/AboutUsPage/AboutUsPage.js
+++ src/components/AboutUsPage/AboutUsPage.js
@@ -10,12 +10,7 @@ const AboutUsPage = () => {
 
   return (
     <div>
-      <h1 className="about_script">MODE is a UK-based, construction services company which focuses mainly on residential and commercial building developments. The company has built reputation for providing reliable, dependable services for high-class quality buildings and provide solutions to complex requirements.
-OUR MISSION
-To deliver high-quality, cost-effective projects on schedule, employing and supporting motivated, flexible and focused teams to achieve the best results. At Mode, we value the importance of our relationships, and we will continue to remain fair in our dealings with all employees, clients and suppliers.
-OUR VALUES
-To be ethical and deliver the best services regarding our client`s expectations.
-Mode values its "one stop" solution approach which means that our clients enjoy the benefits of reduced "gap" management.</h1>
+      <h1 className="about_script">I will change this script</h1>
     </div>
   );
 };
