aa42561b42f0e5d013501d74db0653a8e76da21a
konstantinos koliolios <kolioliosk@gmail.com>
2024-03-05 13:07:40 +0000

    changes in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2023 Mode Construction Services Ltd. All rights reserved. Designed by <a href="https://www.kostaskoliolios.live/" target="_blank" rel="noopener noreferrer">Kostas Koliolios</a></p>
+        <p>&copy; 2024 Mode Construction Services Ltd. All rights reserved. Designed by <a href="https://www.kostaskoliolios.live/" target="_blank" rel="noopener noreferrer">Kostas Koliolios</a></p>
       </div>
     </footer>
   );
